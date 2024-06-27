428844921a97b00a7130726cd70c9bf32326b0ba
heloisebastos <heloisebastos28@hotmail.com>
2024-06-26 20:17:00 -0400

    :tada: Commit inicial

---
+++ src/main/java/br/com/bancohb/produtosbancariospf/ProdutosbancariospfApplication.java
@@ -0,0 +1,13 @@
+package br.com.bancohb.produtosbancariospf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosbancariospfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosbancariospfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bancohb/produtosbancariospf/ProdutosbancariospfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bancohb.produtosbancariospf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosbancariospfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
